5602abfcb81b675cee02e1a3218dd33f9d8faec6
jlmunozpb <jlmunozpb@gmail.com>
2023-12-10 14:53:05 -0500

    main

--- src/components/inmueble/InmuebleCard.js
+++ src/components/inmueble/InmuebleCard.js
@@ -22,8 +22,9 @@ export const InmuebleCard = ({ id,detalle }) => {
                             <div className='card-body'>
                                 <small className='text-muted fw-bolder'>{ detalle.categoria }</small>
                                 <h5 className='card-title'>{ detalle.nombre }</h5>
-                                <h6 className='text-dark fw-bolder'>{ formatterPeso.format(detalle.valor) }&nbsp;</h6>
-                                <h6 className='text-white badge background-main-color position-absolute top-0 translate-middle-x end-0 fw-bolder'><small>{ detalle.tipo }</small></h6>
+                                <h6 className='text-dark fw-bolder'>{ formatterPeso.format(detalle.valor) }&nbsp;<small className='text-muted'>{detalle.tipo === 'Arriendo' ? 'mensual' : ''}</small></h6>
+                                <h6 className='text-white badge background-main-color position-absolute top-50 end-0 fw-bolder mt-5'><small>{ detalle.tipo }</small></h6>
+                                <h6 className={ detalle.estado === 'Disponible' ? 'text-white badge bg-success position-absolute top-0 end-0 fw-bolder' : ( detalle.estado === 'Vendido' ? 'text-white badge bg-danger position-absolute top-0 end-0 fw-bolder' : ( detalle.estado === 'Arrendado' ? 'text-white badge bg-warning position-absolute top-0 end-0 fw-bolder' : '') ) }><small>{ detalle.estado }</small></h6>
                                 <div className='card-text lh-base'>
                                     <table>
                                         <tbody>
