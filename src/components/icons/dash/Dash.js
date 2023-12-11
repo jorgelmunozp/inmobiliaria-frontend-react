400f6afbf927b08d4da57b05efcddfddf1e8cb88
jlmunozpb <jlmunozpb@gmail.com>
2023-12-11 00:08:06 -0500

    main

---
+++ src/components/icons/dash/Dash.js
@@ -0,0 +1,9 @@
+import dash from './dash.svg'
+
+export const Dash = () => {
+    return (
+        <span className=''>
+            <img src={dash} className='iconCard' />
+        </span>
+    );
+}
