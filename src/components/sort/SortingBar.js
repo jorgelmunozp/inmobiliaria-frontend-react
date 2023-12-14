e187f6df29427c25f900aef9a9019a7ebf9e4909
jlmunozpb <jlmunozpb@gmail.com>
2023-12-14 10:43:40 -0500

    main

--- src/components/sort/SortingBar.js
+++ src/components/sort/SortingBar.js
@@ -8,9 +8,9 @@ export const SortingBar = ({ query,inmuebles,inmueblesFiltered,itemPerPage,index
   return (
     <div class="btn-toolbar justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
       <div class="btn-group btn-group-sm" role="group" aria-label="First group">
-        <button type="button" class="button btn btn-outline-ligth border border-muted shadow-sm px-5 text-muted fw-bolder"><small><HomeStar strokeWidth={2} height={'1em'} width={'1em'} /> Más nuevo</small></button>
-        <button type="button" class="button btn btn-outline-ligth border border-muted shadow-sm px-5 text-muted fw-bolder"><small><SortDown strokeWidth={2} /> Menor Precio</small></button>
-        <button type="button" class="button btn btn-outline-ligth border border-muted shadow-sm px-5 text-muted fw-bolder"><small><SortUp strokeWidth={2} /> Mayor Precio</small></button>
+        <button type="button" class="button btn btn-outline-ligth border border-muted shadow-sm px-md-5 text-muted fw-bolder"><small><HomeStar strokeWidth={2} height={'1em'} width={'1em'} /> Más nuevo</small></button>
+        <button type="button" class="button btn btn-outline-ligth border border-muted shadow-sm px-md-5 text-muted fw-bolder"><small><SortDown strokeWidth={2} /> Menor Precio</small></button>
+        <button type="button" class="button btn btn-outline-ligth border border-muted shadow-sm px-md-5 text-muted fw-bolder"><small><SortUp strokeWidth={2} /> Mayor Precio</small></button>
       </div>
     </div>
   )
