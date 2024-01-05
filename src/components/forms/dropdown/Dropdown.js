ffdbd8bdd5d2ae276dfd6da16bbb7005c00d36fc
jlmunozpb <jlmunozpb@gmail.com>
2024-01-05 18:01:58 -0500

    main

--- src/components/forms/dropdown/Dropdown.js
+++ src/components/forms/dropdown/Dropdown.js
@@ -1,7 +1,7 @@
 export const Dropdown = ({ value,query,parameters,setQuery,className }) => {
     return(
       <div className="dropdown form-floating w-100">
-        <button className={className + (query === '' ? ' text-start pt-2' : ' text-center pt-4')} type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+        <button className={className + (query === '' ? ' text-start pt-2 ps-2 ps-sm-3 pe-5' : ' text-center pt-4 ps-2 ps-sm-3 pe-5')} type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
           { query === '' ? value : query }
         </button>
         <label htmlFor="dropdownMenuButton" className="form-label text-muted text-nowrap text-truncate" >
