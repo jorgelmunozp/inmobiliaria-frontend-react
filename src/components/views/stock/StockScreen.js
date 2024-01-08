21ceca6d6a2125ff315e8ac4a3add210d918e465
jlmunozpb <jlmunozpb@gmail.com>
2024-01-08 16:39:27 -0500

    main

--- src/components/views/stock/StockScreen.js
+++ src/components/views/stock/StockScreen.js
@@ -14,9 +14,36 @@ import { MapLocation } from '../../icons/map/MapLocation';
 import { MapPin } from '../../icons/map/MapPin';
 import { Type } from '../../icons/type/Type';
 
+import { PaginationBar } from '../../pagination/PaginationBar';
+
 export const StockScreen = ({ inmuebles }) => {
   const urlBaseBackend = process.env.REACT_APP_URL_BASE_BACKEND;
   const iconSize = 1.5;
+
+  /* Query */
+  let [ query, setQuery ] = useState('');
+  const inmueblesFiltered = [];
+
+  /* Pagination */
+  const [itemPerPage, setItemPerPage ] = useState(10);                 // Se define el número de items por página
+  const [indexPage, setIndexPage ] = useState([0,itemPerPage]);       // Se calculan los indices de la paginación para el filtro Slice(x,y) que entrega un rango de los items de x a y
+  const numPages = ((query === '') ? Math.floor(inmuebles.length/itemPerPage) : Math.floor(inmueblesFiltered.length/itemPerPage));                   // Se calcula la cantidad de páginas = cantidad de items/item por página
+  const resPages = ((query === '') ? inmuebles.length%itemPerPage : inmueblesFiltered.length%itemPerPage);                   // Se calcula la cantidad de páginas = cantidad de items/item por página
+  let indexPages = [];
+  let activePage = [true];                                            // [true]
+  if(resPages !== 0 ){
+    for(let i = 0; i <= numPages; i++) { 
+      indexPages.push(i);                                             // [0,1,2,3]
+      if(i < 0) { activePage.push(false); }                           // [true,false,false,false]
+    }
+  } else if(resPages === 0 ){
+    for(let i = 0; i < numPages; i++) { 
+      indexPages.push(i);                                             // [0,1,2,3]
+      if(i < 0) { activePage.push(false); }                           // [true,false,false,false]
+    }
+  }
+  const [activePages, setActivePages] = useState(activePage);         // [true,false,false,false]
+  
 
   const [ name, setName ] = useState("");
   const [ category, setCategory ] = useState("");
@@ -31,7 +58,6 @@ export const StockScreen = ({ inmuebles }) => {
 
   const handleChangeName = (event) => { setName(event.target.value); };
   const handleChangeCategory = (event) => { setCategory(event.target.value); };
-
 
   const handleStock = () => {
 
@@ -65,7 +91,7 @@ export const StockScreen = ({ inmuebles }) => {
           <span className='col-sm-1 border border-light text-center bg-main-transparent-color'><LocationArrow strokeWidth={0.25} width={iconSize} height={iconSize}/></span>
         </div>
         {
-          inmuebles.map(inmueble => (
+          inmuebles.slice(indexPage[0],indexPage[1]).map(inmueble => (
             <div className='row flex-nowrap' key={inmueble.id}>
               <span className='col-sm-1 py-1 border text-center'> { inmueble.id } </span>
               <div className='image-upload col-auto px-1 border text-center'>
@@ -90,6 +116,7 @@ export const StockScreen = ({ inmuebles }) => {
           ))
         }
       </div>
+      <PaginationBar query={query} array={inmuebles} itemPerPage={itemPerPage} indexPage={indexPage} activePages={activePages} indexPages={indexPages} setIndexPage={setIndexPage} setActivePages={setActivePages} /> 
     </>
   )
 }
