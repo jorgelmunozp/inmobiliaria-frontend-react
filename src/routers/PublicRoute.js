e5b8f698416c68ef4acdee268e54a60cbd563065
jlmunozpb <jlmunozpb@gmail.com>
2024-01-08 18:46:41 -0500

    main

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../auth/authContext';
 import { DashboardRoutes } from './DashboardRoutes';
 
