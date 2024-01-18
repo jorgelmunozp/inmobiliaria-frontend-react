10b06159ae11a10ed57e0909e98e01b7436432e5
jlmunozpb <jlmunozpb@gmail.com>
2024-01-18 16:59:04 -0500

    main

--- src/components/forms/inputs/InputFile.js
+++ src/components/forms/inputs/InputFile.js
@@ -1,12 +1,11 @@
 import { ImageSearch } from '../../icons/image/ImageSearch';
 
-export const InputFile = ({ id,placeholder,acceptFiles='*',file,setFile,className }) => {
-    console.log("file: ",file)
+export const InputFile = ({ id,placeholder,acceptFiles='*',file,setFile,multiple=false,className }) => {
     return (
         <div className={"form-floating text-center"} data-mdb-input-init>
             <div id='input' className={className + ' image-upload img-thumbnail py-3'}>
                 <label htmlFor={ id } style={{"cursor":"pointer"}} ><ImageSearch color={'#aaaaaa'} strokeWidth={1.75} height={1.25} width={1.25} /></label>
-                <input type="file" id={ id } accept={ acceptFiles } onChange={() => setFile(document.getElementById( id ).files[0]) } />
+                <input type="file" id={ id } accept={ acceptFiles } onChange={() => setFile(document.getElementById( id ).files[0]) } multiple={ multiple } />
                 <label className='text-muted ms-2'>{file.name}</label>
             </div>
             <label htmlFor='input' className="form-label text-muted text-nowrap text-truncate" >{ placeholder }</label>
--- src/components/views/inmueble/InmuebleUpload.js
+++ src/components/views/inmueble/InmuebleUpload.js
@@ -8,6 +8,7 @@ import { HomeThumbnail } from '../../icons/home/HomeThumbnail';
 export const InmuebleUpload = ({ inmuebles, urlApiInmuebles, urlBaseFrontend, categorias, tipos, estados }) => {
   const [ code, setCode ] = useState( inmuebles.slice(-1)[0].id + 1 ); 
   const [ image, setImage ] = useState(''); 
+  const [ name, setName ] = useState(''); 
   const [ category, setCategory ] = useState(''); 
   const [ type, setType ] = useState(''); 
   const [ state, setState ] = useState(''); 
@@ -25,6 +26,7 @@ export const InmuebleUpload = ({ inmuebles, urlApiInmuebles, urlBaseFrontend, ca
 
   console.log("code: ",code)
   console.log("image: ",image)
+  console.log("image: ",name)
   console.log("category: ",category)
   console.log("type: ",type)
   console.log("state: ",state)
@@ -44,84 +46,69 @@ export const InmuebleUpload = ({ inmuebles, urlApiInmuebles, urlBaseFrontend, ca
       <hr />
       <center><h5>Subir Inmueble</h5></center> 
       <hr />
-      <div className="container">
-        <div className='row'>
-          <div className='col'>
+      <center><HomeThumbnail color={'#aaaaaa'} height={2} width={2} /></center>
+      <div className="container mt-3">
+        <div className='row d-block d-sm-flex'>
+          <div className='col my-2'>
             <InputNumber placeholder={'Código'} id={'codigo'} value={code} disabled onInputChange={(values) => setCode(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
-        </div>
+          <div className="col my-2">
+            <InputFile id={'image-primary'} placeholder={'Imagen'} multiple={false} acceptFiles={"image/*"} file={image} setFile={setImage} className="input form-control border-muted text-muted shadow-sm"/>
+          </div>
 
-        <div className='row'>
-          <div className="col">
-            <InputFile id={'input-image'} placeholder={'Imagen'} acceptFiles={"image/*"} file={image} setFile={setImage} className="input form-control border-muted text-muted shadow-sm"/>
+          <div className='col my-2'>
+            <InputText id={'nombre'} placeholder={'nombre'} inputText={name} onInputChange={(values) => setName(values.target.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
         </div>
-        <div className='row'>
-          <div className='col'>
+        <div className='row d-block d-sm-flex'>
+          <div className='col my-2'>
+            <InputNumber prefix={'$ '} placeholder={'Valor'} id={'valor'} value={value} onInputChange={(values) => setValue(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
+          </div>
+          <div className='col my-2'>
             <Dropdown placeholder={'Categoría'} query={category} parameters={categorias.slice(1,categorias.length)} setQuery={setCategory} className={"input form-control rounded border-muted border-1 text-muted shadow-sm"}/>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
+          <div className='col my-2'>
             <Dropdown placeholder={'Tipo negocio'} query={type} parameters={tipos.slice(1,tipos.length)} setQuery={setType} className={"input form-control rounded border-muted border-1 text-muted shadow-sm"}/>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
-            <Dropdown placeholder={'Estado'} query={state} parameters={estados} setQuery={setState} className={"input form-control rounded border-muted border-1 text-muted shadow-sm"}/>
+          <div className='col my-2'>
+            <Dropdown placeholder={'Estado'} query={state} parameters={estados} setQuery={setStatus} className={"input form-control rounded border-muted border-1 text-muted shadow-sm"}/>
           </div>
         </div>
-        <div className='row'>
-          <div className='col'>
+        <div className='row d-block d-sm-flex'>
+          <div className='col my-2'>
             <InputNumber placeholder={'Habitaciones'} id={'habitaciones'} value={rooms} onInputChange={(values) => setRooms(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
+          <div className='col my-2'>
             <InputNumber placeholder={'Baños'} id={'baños'} value={bathrooms} onInputChange={(values) => setBathooms(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
+          <div className='col my-2'>
             <InputNumber placeholder={'Parqueaderos'} id={'parqueaderos'} value={garages} onInputChange={(values) => setGarages(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
+          <div className='col my-2'>
             <InputNumber placeholder={'Área'} id={'area'} value={area} onInputChange={(values) => setArea(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
         </div>
-        <div className='row'>
-          <div className='col'>
-            <InputNumber prefix={'$ '} placeholder={'Valor'} id={'valor'} value={value} onInputChange={(values) => setValue(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
-            <InputText id={'descripcion'} placeholder={'Descripción'} inputText={description} onInputChange={(values) => setDescription(values.target.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
+        <div className='row d-block d-sm-flex'>
+          <div className='col my-2'>
             <InputText id={'ciudad'} placeholder={'Ciudad'} inputText={city} onInputChange={(values) => setCity(values.target.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
+          <div className='col my-2'>
             <InputText id={'sector'} placeholder={'Sector'} inputText={neighborhood} onInputChange={(values) => setNeighborhood(values.target.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
+          <div className='col my-2'>
             <InputNumber placeholder={'Estrato'} id={'estrato'} value={stratum} onInputChange={(values) => setStratum(values.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
         </div>
         <div className='row'>
-          <div className="col">
-            <InputFile id={'input-image'} placeholder={'Imagenes'} acceptFiles={"image/*"} file={images} setFile={setImages} className="input form-control border-muted text-muted shadow-sm"/>
+          <div className='col my-2'>
+            <InputText id={'descripcion'} placeholder={'Descripción'} inputText={description} onInputChange={(values) => setDescription(values.target.value)} className={'input form-control rounded border-muted border-1 align-bottom text-muted text-center px-2 shadow-sm w-100'}/>
           </div>
         </div>
-
+        <div className='row'>
+          <div className="col my-2">
+            <InputFile id={'images-secondary'} placeholder={'Imagenes'} multiple={true} acceptFiles={"image/*"} file={images} setFile={setImages} className="input form-control border-muted text-muted shadow-sm"/>
+          </div>
+        </div>
       </div>
     </>
   )
