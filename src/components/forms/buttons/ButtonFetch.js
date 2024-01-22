ddedd4164c877356eadcee651612e42f007a7ce0
jlmunozpb <jlmunozpb@gmail.com>
2024-01-22 15:59:29 -0500

    main

--- src/components/forms/buttons/ButtonFetch.js
+++ src/components/forms/buttons/ButtonFetch.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Button } from './Button';
 import { fetchCreate } from '../../../helpers/fetchCreate';
+import Swal from 'sweetalert2';
+import {  myColor } from '../../../global';
 
 export const ButtonFetch = ({ color,icon,title,urlApi,contenidoApi,setResponseStatus,createFlag,className }) => {
   return (
     <Button icon={icon} title={title} type={1} className={className + ' py-4 w-100'} 
-            onClick={ () => fetchCreate(urlApi,contenidoApi,setResponseStatus,createFlag) } />
-
+            onClick={ () => { createFlag 
+                                ? fetchCreate(urlApi,contenidoApi,setResponseStatus,createFlag)
+                                : Swal.fire({
+                                  title: "Aún no",
+                                  text: "Debes llenar primero todos los campos",
+                                  icon: "warning",
+                                  confirmButtonColor: myColor,
+                                })
+          
+          } } />
   )
 }
