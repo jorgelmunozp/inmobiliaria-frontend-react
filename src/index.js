894c3cfa702734dc97b858622a1d4ecd39eedae5
jlmunozpb <jlmunozpb@gmail.com>
2024-06-30 11:25:48 -0500

    main

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './assets/styles/styles.css';
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>,
+  </React.StrictMode>,
 );
