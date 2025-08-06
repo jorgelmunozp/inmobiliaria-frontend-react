1238f66fc663b16ce383a0f9809e42287738a3f0
jlmunozpb <jlmunozpb@gmail.com>
2025-08-06 16:12:28 -0500

    main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
 import './assets/styles/styles.css';
+import { Suspense, lazy } from "react";
+import { createRoot } from 'react-dom/client';
+import { Logo } from './components/icons/logo/Logo.js';
 
+const App = lazy(() => import('./App.js'));
 const root = createRoot(document.getElementById('root'));
-root.render( <App /> );
+root.render(
+  <Suspense delayMs={0} fallback={ <div style={{ alignItems:'center', display:'flex', height:'100vh', justifyContent:'center', width:'100%', animation:'splash 0.75s linear infinite' }}><Logo color={'#5285c5'} height={3.5} width={7.25} /></div> }>
+    <App Logo={Logo} />
+  </Suspense>
+);
